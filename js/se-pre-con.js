26f55eeb9ce53c0f5bd9de2a1a773880ffde9c15
stickerbrushes <miltong0830@gmail.com>
2021-04-22 18:36:46 -0400

    Update se-pre-con.js
    $(window).load(function() {
    has to change to
    $(window).on("load",function() {
    
    It was changed in 3.0.
    
    Source: https://forum.jquery.com/topic/jquery-moving-from-1-10-2-to-3-3-1#14737000007942007

--- js/se-pre-con.js
+++ js/se-pre-con.js
@@ -3,7 +3,7 @@
 
 //paste this code under the head tag or in a separate js file.
 	// Wait for window load
-	$(window).load(function() {
+	$(window).("load",function() {
 		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");;
 	});
