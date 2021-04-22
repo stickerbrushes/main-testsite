5ce6d650b9fbb86cff519bef8f63596b75670080
stickerbrushes <miltong0830@gmail.com>
2021-04-21 22:17:21 -0400

    Script was calling to HTML (DOCTYPE HTML)
    I read this: To add to @andy_magoon, in my case, I had a script tag that was supposed to serve up javascript, but because the request was redirected to an HTML page (its not important why it was redirected), which begins with <!DOCTYPE html>, which is not valid javascript, the browser returns the SyntaxError when it attempt to parse the HTML as JS
    
    Source: https://stackoverflow.com/questions/3143698/uncaught-syntaxerror-unexpected-token#comment60417768_26615223

--- js/se-pre-con.js
+++ js/se-pre-con.js
@@ -1,7 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
-<script type="text/javascript">
 
 //paste this code under the head tag or in a separate js file.
 	// Wait for window load
