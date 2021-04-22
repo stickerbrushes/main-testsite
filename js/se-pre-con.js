55840c83a6070d08055c5570d12168a2d9abb2d8
stickerbrushes <miltong0830@gmail.com>
2021-04-21 22:26:36 -0400

    Update se-pre-con.js

--- js/se-pre-con.js
+++ js/se-pre-con.js
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
 
 //paste this code under the head tag or in a separate js file.
 	// Wait for window load
